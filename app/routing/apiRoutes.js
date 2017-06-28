5dd4e6466f736220a121c9774dc632345300621b
emilypo89 <emilypo89@gmail.com>
2017-06-28 18:47:20 -0400

    pulls scores correctly and compares the scores within the different arrays

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,11 +20,41 @@ module.exports = function(app) {
 	app.post("/api/friends", function(req, res) {
 	  var newFriend = req.body;
 
-	  console.log(newFriend);
+	  console.log(newFriend.scores);
 	  // console.log(friends);
-
+	  var newFriendScores = newFriend.scores;
 	  friends.push(newFriend);
+	  console.log(friends);
+	  console.log("New Friend Scores: " + newFriendScores);
+	  
+			var friendsScores = [];
 
+			for(i = 0; i < friends.length; i ++){
+				friendsScores = friends[i].scores;
+				console.log("Friend scores: " + friendsScores);
+
+
+				var result = friendsScores.map(function (num, idx) {
+				  return Math.abs(num) - Math.abs(newFriendScores[idx]);
+				});
+				console.log("new array: " + result);
+
+				var sum = result.reduce(add, 0);
+
+				function add(a, b) {
+				    return a + b;
+				}
+
+				console.log(Math.abs(sum));
+
+				// console.log("Difference: " + numberarr2);
+			}
+
+			
+		
+			
+			
+			
 
 	 // compareScores(newFriend);
 
@@ -32,13 +62,7 @@ module.exports = function(app) {
 	});
 
 // function compareScores (newFriend) {
-// 	var newFriendScores = newFriend.scores;
-// 	var friendsScores = [];
-// 	for(i = 0; i < friends.length; i ++);
-		
-// 	friendsScores = friends[i].scores;
-// 	 console.log(newFriendScores);
-// 	 console.log(friendsScores);
+
 // }	
 
 
